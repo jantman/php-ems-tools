e063165d7ef989bf3cfa31e631a80f444dfae0b9
Jason Antman <jason@jasonantman.com>
2007-09-15 00:50:54 +0000

    Added standard headers. (issue: 12)

--- inc/antman.php
+++ inc/antman.php
@@ -1,6 +1,35 @@
 <?php
-//antman.php 
-//Jason Antman Common PHP functions
+// inc/antman.php
+//
+// A few simple functions commonly used by the developer.
+//
+// +----------------------------------------------------------------------+
+// | PHP EMS Tools	http://www.php-ems-tools.com                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2006, 2007 Jason Antman.	                          |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 3 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// |Please use the above URL for bug reports and feature/support requests.|
+// +----------------------------------------------------------------------+
+// | Authors: Jason Antman <jason@jasonantman.com>                        |
+// +----------------------------------------------------------------------+
+//	$Id$
 
 function textMonth($number)
 {
--- inc/notify.php
+++ inc/notify.php
@@ -1,10 +1,36 @@
 <?php 
-// PHP EMS Tools
-// Email Notification Module
-// Time-stamp: "2007-09-13 16:19:08 jantman"
+// inc/notify.php
 //
+// Handles email/SMS notification for schedule changes.
+//
+// +----------------------------------------------------------------------+
+// | PHP EMS Tools	http://www.php-ems-tools.com                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2006, 2007 Jason Antman.	                          |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 3 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// |Please use the above URL for bug reports and feature/support requests.|
+// +----------------------------------------------------------------------+
+// | Authors: Jason Antman <jason@jasonantman.com>                        |
+// +----------------------------------------------------------------------+
+//	$Id$
 
-// this file will import the user's customization
 require('./config/config.php');
 
 function schedule_edit_mail($year, $month, $date, $shift, $EMTid, $start, $end)
