7dfad748b75c474a0cf342543c17485dad4cf884
Jason Antman <jason@jasonantman.com>
2007-09-15 00:21:23 +0000

    updated to standard headers (issue: 12)

--- config/config.php
+++ config/config.php
@@ -1,16 +1,35 @@
 <?php
-
-//(C) 2006 Jason Antman. All Rights Reserved.
-// with questions, go to www.jasonantman.com
-// or email jason AT jasonantman DOT com
-// Time-stamp: "2007-09-13 16:26:30 jantman"
-
-//This software may not be copied, altered, or distributed in any way, shape, form, or means.
-// version: 0.1 as of 2006-10-3
-
-// INFORMATION:
-// this is the customization file attached to the php_ems package
-// this file MUST be in place for the packages to work.
+// config/config.php
+//
+// Main configuration file.
+//
+// +----------------------------------------------------------------------+
+// | PHP EMS Tools      http://www.php-ems-tools.com                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2006, 2007 Jason Antman.                               |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 3 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// |Please use the above URL for bug reports and feature/support requests.|
+// +----------------------------------------------------------------------+
+// | Authors: Jason Antman <jason@jasonantman.com>                        |
+// +----------------------------------------------------------------------+
+//      $Id$
 
 // INSTRUCTIONS:
 // set the variables in the file to the appropriate values for your squad.
