6e1586fd77c367e923a58c54e901e070617723ce
Jason Antman <jason@jasonantman.com>
2007-09-15 03:32:09 +0000

    added standard header, removed MPAC hard coding and changed to using the proper variables from config.php (issue: 20)

--- external/auth/index.php
+++ external/auth/index.php
@@ -1,43 +1,56 @@
+<?php
+// external/auth/index.php
+//
+// Index page for external (authenticated) pages.
+//
+// +----------------------------------------------------------------------+
+// | PHP EMS Tools      http://www.php-ems-tools.com                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2006, 2007 Jason Antman.                               |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 3 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// |Please use the above URL for bug reports and feature/support requests.|
+// +----------------------------------------------------------------------+
+// | Authors: Jason Antman <jason@jasonantman.com>                        |
+// +----------------------------------------------------------------------+
+//      $Id$
+
+require_once('../../config/config.php');
+?>
+
 <html>
 <head>
-<title>MPAC Internet Schedule View</title>
+
+<?php
+global $shortName;
+global $orgName;
+echo '<title>'.$shortName.' Internet Schedule View</title>';
+?>
 </head>
 <body>
-<h2>Midland Park Ambulance Corps - Internet Schedule View</h2>
-<p>
-<a href="schedule.php">View Schedule</a>
-</p><p>
-<a href="dispatchSchedule.php">Dispatch Schedule</a>
-</p><p>
-<a href="saturdays.html">Saturday Night Schedule 2006 - January - June</a>
-</p><p>
-<a href="roster.php">Roster</a>
-</p><p>
 <?php
-// DB Connection
-$conn = mysql_connect()   or die("Error. MySQL connection failed at mysql_connect"); 
-mysql_select_db('pcr') or die ('Unable to select database!');
-$query  = "SELECT RunNumber,Date FROM calls WHERE YEAR(Date)=".date("Y").";";
-$result = mysql_query($query) or die ("Error in query: $query. " . mysql_error());
-$count = 0;
-while($row = mysql_fetch_array($result))
-{
-	$count++;
-}
-mysql_free_result($result);
-echo "<b>Calls for ".date("Y")." : ".$count."</b><br>";
-
-$query  = "SELECT RunNumber,Date FROM calls WHERE YEAR(Date)=".date("Y")." && MONTH(Date)=".date("n").";";
-$result = mysql_query($query) or die ("Error in query: $query. " . mysql_error());
-$count = 0;
-while($row = mysql_fetch_array($result))
-{
-	$count++;
-}
-mysql_free_result($result);
-echo "</p><p>";
-echo "<b>Calls for ".date("F Y")." : ".$count."</b><br>";
+echo '<h2>'.$orgName.' - Internet Schedule View</h2>';
 ?>
-</p>
+
+<p><a href="schedule.php">View Schedule</a></p>
+<p><a href="dispatchSchedule.php">Dispatch Schedule</a></p>
+<p><a href="roster.php">Roster</a></p>
+
 </body>
 </html>
